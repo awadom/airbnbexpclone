4b6a70cc70efe4a4065adfb208ec4853d1860e80
denfushi <awadomar@gmail.com>
2022-12-03 16:27:46 -0800

    content component with photo grid h1 and p

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Navbar from "./components/Navbar.js";
-import "./style.css";
+import Content from "./components/Content.js";
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <Content />
     </>
   );
 }
---
+++ src/components/Content.js
@@ -0,0 +1,12 @@
+export default function Content() {
+  return (
+    <div className="content">
+      <img className="content-img" src="/images/photo-grid.png" alt="" />
+      <h1>Online Experiences</h1>
+      <p>
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
+    </div>
+  );
+}
