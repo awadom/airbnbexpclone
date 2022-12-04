c5f9a43b0b69bea0ed59a4139d23c76dad605764
denfushi <awadomar@gmail.com>
2022-12-03 22:05:56 -0800

    card component complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Navbar from "./components/Navbar.js";
 import Card from "./components/Card.js";
-//import Hero from "./components/Hero.js";
-// <Hero />
+import Hero from "./components/Hero.js";
+
 export default function App() {
   return (
     <>
       <Navbar />
+      <Hero />
       <Card />
     </>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,20 @@
 export default function Card() {
   return (
     <section className="card">
-      <div className="card-status-img">SOLD OUT</div>
+      <div className="card-status-img">
+        <div className="card-status">SOLD OUT</div>
+        <img className="card-img" src="./images/katie-zaferes.png" alt="" />
+      </div>
       <div className="card-rating">
         <img src="./images/star.png" alt="" />
         <p>5.0</p>
-        <p>(6) · USA</p>
+        <p className="card-rating-amt-country">(6) · USA</p>
       </div>
       <p>Life lessons with Katie Zaferes</p>
-      <p>From $136</p>
-      <p>/ person</p>
+      <div className="card-price">
+        <p className="card-price-cost">From $136</p>
+        <p>/ person</p>
+      </div>
     </section>
   );
 }
